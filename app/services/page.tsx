4a43716e42795e2abc3e977fd485b5e6800b6098
munnakumar046 <munnakumar15006@gmail.com>
2026-05-08 15:55:38 +0530

    service text moved to h1 tag

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,5 +1,9 @@
 const ServicesPage = () => {
-  return <div className="text-8xl">ServicesPage</div>;
+  return <div className="text-8xl">
+    <h1>
+      ServicesPage
+    </h1>
+  </div>;
 };
 
 export default ServicesPage;
